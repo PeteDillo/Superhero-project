2bfdca9369d50684f086187ac6be3e0f656b0883
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2021-10-21 14:17:30 -0500

    Finished the install for django

--- superhero_project/superhero_project/asgi.py
+++ superhero_project/superhero_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for superhero_prpject project.
+ASGI config for superhero_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'superhero_prpject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'superhero_project.settings')
 
 application = get_asgi_application()
